bbd1343334efb4bf4cfcaf90c935bf9f7705b37e
FadhilahSyafa20 <58615696+FadhilahSyafa20@users.noreply.github.com>
2020-06-06 12:53:39 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+            <meta charset="utf-8">
+                <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport">
+                    <link href="assets\bootstrap\css\bootstrap.min.css" rel="stylesheet">
+                        <link href="assets\css\fontawesome-free\css\all.min.css" rel="stylesheet">
+                            <link href="assets\css\style.css" rel="stylesheet">
+                                <title>
+                                    Data Editor
+                                </title>
+                            </link>
+                        </link>
+                    </link>
+                </meta>
+            </meta>
+        </meta>
+    </head>
+    <body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand font-weight-bold" href="index.php">
+                <i class="fas fa-edit">
+                </i>
+                DATA EDITOR
+            </a>
+            <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler" data-target="#navbarSupportedContent" data-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon">
+                </span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="cari.php">
+                            PENCARIAN EDITOR
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="tampil.php">
+                            TAMPILKAN EDITOR
+                        </a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="about.php">
+                            ABOUT ME
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+        <div class="container mt-5">
+            <form method="post">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="profile-img">
+                            <img src="assets\img\photo.jpg" class="img-thumbnail" alt="Cinque Terre" width="280" height="100"> 
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="profile-head">
+                                    <h1>
+                                        Fadhilah Syafa
+                                    </h1>
+                                    <h4>
+                                        1808107010016
+                                    </h4><br/>
+                                    <p class="proile-rating">Banda Aceh, Indonesia </p><br/>
+                            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Profile</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Social</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+
+                    </div>
+                    <div class="col-md-8">
+                        <div class="tab-content profile-tab" id="myTabContent">
+                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Nama</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Fadhilah Syafa</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Tempat / Tgl Lahir</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Banda Aceh, 20 Januari 2000</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Status</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Mahasiswa</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Universitas</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Universitas Syiah Kuala</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Fakultas</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>Matematika dan Ilmu Pengetahuan Alam</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Jurusan</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>S1 Informatika</p>
+                                            </div>
+                                        </div>
+                            </div>
+                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Email</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p>fadhila18@mhs.unsyiah.ac.id</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Facebook</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p><a href="https://www.facebook.com/fadhilah.syafa/">Fadhilah Syafa</a></p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Instagram</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p><a href="https://www.instagram.com/fadhilah20s/">@fadhilah20s</a></p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <label>Github</label>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <p><a href="https://github.com/FadhilahSyafa20">FadhilahSyafa20</a></p>
+                                            </div>
+                                        </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>           
+        </div>
+        <script src="assets\bootstrap\js\jquery.min.js">
+        </script>
+        <script src="assets\bootstrap\js\bootstrap.min.js">
+        </script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets\bootstrap\css\bootstrap.min.css">
+    <link rel="stylesheet" href="assets\css\fontawesome-free\css\all.min.css">
+    <link rel="stylesheet" href="assets\css\style.css">
+	<title>Data Editor</title>
+</head>
+<body>
+	<!-- Navbar -->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+	  <a class="navbar-brand font-weight-bold" href="#"><i class="fas fa-edit"></i> DATA EDITOR</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav ml-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="cari.php">PENCARIAN EDITOR</a>
+	      </li>
+	      <li class="nav-item active">
+	        <a class="nav-link" href="tampil.php">TAMPILKAN EDITOR</a>
+	      </li>
+	      <li class="nav-item active">
+	        <a class="nav-link" href="about.php">ABOUT ME</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+	<!-- End Navbar -->
+
+	<!-- Jumbotron -->
+	<div class="jumbotron mb-0">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="jumbotron flex-grow-0 flex-shrink-1" id="gateway">
+		  	<h1 class="display-3 text-center">Selamat Datang!</h1>
+			<p class="lead text-center">Website untuk menampilkan informasi editor pada artikel situs detik.com beserta artikel yang telah diedit<br>Data yang diperoleh berasal dari struktur data hash yang diintegrasi dengan Web</p>
+			<p class="text-center"><a class="btn btn-primary btn-lg" href="https://github.com/mr1dha/java-hash-structure-implemented-to-web" style="display:block;">Kunjungi Github →</a></p>
+			<hr class="my-4">
+			<p >Website ini dibuat untuk memenuhi tugas Projek Lab Matakuliah Struktur Data dan Algoritma Program Studi Informatika Unsyiah</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+	<!-- End Jumbotron -->
+
+	<script src="assets\bootstrap\js\jquery.min.js"></script>
+    <script src="assets\bootstrap\js\bootstrap.min.js"></script>
+
+</body>
+</html>
